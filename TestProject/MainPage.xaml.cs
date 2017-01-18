915fa7d640d770a6c12c65d2d14cbc54a3f0a328
David Navrkal <navrkal@master.cz>
2017-01-18 14:49:23 +0100

    initial commit

---
+++ TestProject/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.Storage.Streams;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using System.Threading.Tasks;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaCapture m_mediaCapture;
+        private InMemoryRandomAccessStream m_mediaStream;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            m_mediaCapture = new MediaCapture();
+            m_mediaStream = new InMemoryRandomAccessStream();
+            //VideoDeviceController.GetAvailableMediaStreamProperties
+        }
+
+        
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            await m_mediaCapture.InitializeAsync();
+            MediaEncodingProfile encodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Qvga);
+            //encodingProfile.Video.Subtype = "H264ES";
+            //m_mediaCapture.SetEncoderProperty(MediaStreamType.VideoRecord, IMFAttributes. System.Guid.Parse("H264"), );
+
+            //MediaEncodingSubtypes.H264Es
+            //m_mediaCapture.En
+            //m_mediaCapture.GetEncoderProperty();
+            encodingProfile.Audio = null;
+            //await m_mediaCapture.SetEncodingPropertiesAsync(MediaStreamType.VideoRecord, IMediaEncodingProperties.);
+            await m_mediaCapture.StartRecordToStreamAsync(encodingProfile, m_mediaStream);
+            System.Diagnostics.Debug.WriteLine(m_mediaCapture.ToString());
+        }
+
+        private async void button1_Click(object sender, RoutedEventArgs e)
+        {
+            await m_mediaCapture.StopRecordAsync();
+            m_mediaStream.Seek(0);
+            byte[] bytes = new byte[m_mediaStream.Size];
+            await m_mediaStream.AsStream().ReadAsync(bytes, 0, bytes.Length);
+            string outStr = BitConverter.ToString(bytes);
+            System.Diagnostics.Debug.WriteLine(outStr);
+            SaveToFile();
+        }
+
+        private async void SaveToFile()
+        {
+            FileSavePicker videoSavePicker = new FileSavePicker();
+            videoSavePicker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            videoSavePicker.FileTypeChoices.Add(new KeyValuePair<string, IList<string>>("Video", new List<string>() { ".mp4" }));
+            var mediaFile = await videoSavePicker.PickSaveFileAsync();
+            m_mediaStream.Seek(0);
+            byte[] result = new byte[m_mediaStream.Size];
+            //await _mediaStream.ReadAsync(result, _mediaStream.Size, 0);
+            await SaveStreamToFileAsync(m_mediaStream, mediaFile);
+        }
+
+        private static async Task SaveStreamToFileAsync(IRandomAccessStream stream, StorageFile mediaFile)
+        {
+            if (mediaFile != null)
+            {
+                using (var dataReader = new DataReader(stream.GetInputStreamAt(0)))
+                {
+                    await dataReader.LoadAsync((uint)stream.Size);
+                    byte[] buffer = new byte[(int)stream.Size];
+                    dataReader.ReadBytes(buffer);
+                    await FileIO.WriteBytesAsync(mediaFile, buffer);
+                }
+            }
+        }
+    }
+}
